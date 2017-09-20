1f81cc36300b126d1eb9971f91f01b730d7b7bba
ValentinMizev <valentin.mizev@gmail.com>
2017-09-20 15:34:19 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,65 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+    switch (req.url) {
+        case "/file": {
+          switch (req.method){
+            case "POST": {
+              var chunks = [];
+              req.on("data", function(chunk){
+                  chunks.push(chunk);
+                })
+                .on("end", function(){
+                  var data = Buffer.concat(chunks).toString();
+                  fs.writeFile('file.txt', data, function (err) {
+                    if (err) throw err;
+                    console.log('Saved new data to file!');
+                  });
+                })              
+            } break;
+            case "GET": {
+              fs.readFile('file.txt','utf8', (err, data) => {
+                if (err) throw err;
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.write(JSON.stringify({
+                  text: data
+                }));
+                res.end();
+              });
+            } break;
+            case "DELETE": {
+              fs.truncate('file.txt', 0, function(){
+                console.log('Data Cleared!')
+              })
+            }break;
+            case "PUT": {
+              var chunks = [];
+              req.on("data", function(chunk){
+                chunks.push(chunk);
+              })
+              .on("end", function(){
+                var data = Buffer.concat(chunks).toString();
+                fs.appendFile('file.txt', data, function (err) {
+                  if (err) throw err;
+                  console.log('Added text to the end of file!');
+                });
+              });
+            } break;
+            default:{
+              res.statusCode = 404;
+              res.setHeader("Content-Type", "text/plain");
+              res.end("Not found!");
+            }
+          }
+        } break;
+        default: {
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/plain");
+          res.end("Not found!");
+        }
+    }
+  })
+  .listen(3000, '127.0.0.1', function(){
+      console.info('Server running at 127.0.0.1:3000')
+  })
